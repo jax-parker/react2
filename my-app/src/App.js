9a72364fee813fb2cc0560d760b5b70146d2bf72
jax-parker <parkyjax711@gmail.com>
2023-08-22 13:55:44 +0000

    functional Greeting

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreetingWithProps />
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FunctionalGreeting() {
+    return <h1>Hello</h1>
+}
+//const FunctionalGreeting = () => <h1>Hello</h1>;
+export default FunctionalGreeting;
