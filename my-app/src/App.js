d4f3384c9d2d345f5bb4e5e3ede014d260b7eb4d
jax-parker <parkyjax711@gmail.com>
2023-09-02 17:21:41 +0000

    component will unmount

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import LifeCyclesCDM from "./components/LifeCyclesCDM";
+import LifeCyclesCWU from "./components/LifeCyclesCWU";
 
 function App() {
   return <div className="App">
-      <LifeCyclesCDM />
-  </div>;
+      <LifeCyclesCWU />
+    </div>;
 }
 
 export default App;
