edac79886f3230800e45fba7174364255ca8b5b0
jax-parker <parkyjax711@gmail.com>
2023-09-07 10:20:03 +0000

    use effect counter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
-// import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState";
-// import HooksCounter from "./components/HooksCounter";
-import UseStateWithObjects from "./components/UseStateWithObjects";
+import UseEffectCounter from "./components/UseEffectCounter";
+
 
 
 function App() {
   return <div className="App">
-      {/* <StatefulGreetingWithPrevState />
-      <HooksCounter /> */}
-      <UseStateWithObjects />
+      {
+        <UseEffectCounter />
+      }
   </div>;
 }
 
